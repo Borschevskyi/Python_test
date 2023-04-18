8b9e92b4951b76f0e6273e869db4a32b6f2a3c13
Borschevskyi <Borsch2556@gmail.com>
2023-04-18 16:32:27 +0300

    fix registration

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-
 from flask import Flask, request, session, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
+from flask_migrate import Migrate
 from wtforms import StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired, EqualTo
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-users = {'admin': {'password': 'admin'}
-         }
-
-
+users = {}
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm_password')])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
-    
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -29,6 +28,9 @@ class User(db.Model, UserMixin):
 
     def is_active(self):
         return True
+
+    def get_id(self):
+        return str(self.id)
 
 login_manager = LoginManager()
 
@@ -42,6 +44,9 @@ def load_user(user_id):
     user.id = user_id
     return user
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def index():
@@ -49,22 +54,19 @@ def index():
         return f'Logged in as {session["username"]}<br><a href="/logout">Logout</a>'
     return 'You are not logged in<br><a href="/login">Login</a>'
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
-        if username in users and password == users[username]['password']:
-            user = User()
-            user.id = username
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
             login_user(user)
             session['username'] = username
             return redirect(url_for('index'))
         else:
             return render_template('login.html', error='Incorrect username or password')
-
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -73,12 +75,14 @@ def register():
     if form.validate_on_submit():
         username = form.username.data
         password = generate_password_hash(form.password.data)
+        # Проверяем, что пользователь с таким именем не существует
+        if User.query.filter_by(username=username).first() is not None:
+            return render_template('register.html', form=form, error='Username already taken')
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
-
 
 @app.route('/logout')
 def logout():
@@ -86,12 +90,10 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
 @app.route('/profile')
 @login_required
 def profile():
     return f'Hello, {current_user.id}!'
-
 
 if __name__ == '__main__':
     app.run(host="localhost", port="8000", debug=True)
